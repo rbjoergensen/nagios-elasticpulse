b4390d930d741f7e7e279f197a82be18c10e65cf
rbjoergensen <rbjoergensen@users.noreply.github.com>
2017-06-19 22:44:23 +0200

    Initial

---
+++ elasticpulse_indexcount.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+curl -XGET -u username:password 'elasticsearch.domain.com/replace_with_indexname/replace_with_type/_search' -H 'Content-Type: application/json' -d '{"query": { "range" : { "indexdate" : { "gte" : "now-15m", "lt" : "now"}}}}' | python -c "import sys, json; print json.load(sys.stdin)['hits']['total']"
+echo "Index Count: $index_count"
+#Debug value if you should need to check functionality.
+#index_count=-11
+if ((5<=$index_count))
+then
+echo "OK - Index count is $index_count."
+exit 0
+elif ((1<=$index_count && $index_count<=4))
+then
+echo "WARNING - Index count is $index_count."
+exit 1
+elif ((0==$index_count))
+then
+echo "CRITICAL - Index count is $index_count."
+exit 2
+else
+echo "UNKNOWN - Index count is $index_count."
+exit 3
+fi
